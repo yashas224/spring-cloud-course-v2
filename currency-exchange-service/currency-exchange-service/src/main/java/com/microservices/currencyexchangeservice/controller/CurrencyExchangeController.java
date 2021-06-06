8790a3fe7c71bd8d4036b43387a9171948af1ac8
yashas224 <71977329+yashas224@users.noreply.github.com>
2021-06-06 05:34:44 +0530

    update
    update

--- currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,10 +1,7 @@
 package com.microservices.currencyexchangeservice.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
